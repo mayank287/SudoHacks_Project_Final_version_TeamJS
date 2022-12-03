137230522a6d094768037d75dc828203fa62205f
Your Name <mayank2157@gmail.com>
2022-12-03 12:47:19 +0530

    Migrate to vercel

---
+++ client/build/precache-manifest.a7cd9320916829a727f92ee106246a8e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e324aa7b7089013e8eae5586865eef61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae430fea6db5e8bcd829",
+    "url": "/static/css/2.c04e441f.chunk.css"
+  },
+  {
+    "revision": "03191d7a66a258bc9367",
+    "url": "/static/css/main.25db8988.chunk.css"
+  },
+  {
+    "revision": "ae430fea6db5e8bcd829",
+    "url": "/static/js/2.e6b8b0fb.chunk.js"
+  },
+  {
+    "revision": "42c1eadcacf65f8e01ed32ea64e353fc",
+    "url": "/static/js/2.e6b8b0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03191d7a66a258bc9367",
+    "url": "/static/js/main.87b26318.chunk.js"
+  },
+  {
+    "revision": "3bd14320f348bfcfa0c0",
+    "url": "/static/js/runtime-main.fc49f826.js"
+  },
+  {
+    "revision": "5d35e39988e3a183bdc3a9d2570d20a9",
+    "url": "/static/media/Noprofile.5d35e399.gif"
+  },
+  {
+    "revision": "c4335da96fa2592616d940aa47ce70df",
+    "url": "/static/media/calendar.c4335da9.svg"
+  },
+  {
+    "revision": "b6dd53fdce8458e6637232ca15ede2b1",
+    "url": "/static/media/coughing_2.b6dd53fd.svg"
+  },
+  {
+    "revision": "02c2292dd715cb36b47899d1d184894d",
+    "url": "/static/media/doctor (1).02c2292d.png"
+  },
+  {
+    "revision": "0211ccbeea09e876acc403951f2bf237",
+    "url": "/static/media/doctor4.0211ccbe.svg"
+  },
+  {
+    "revision": "e99223a0754858c2cb8dc760d75443ca",
+    "url": "/static/media/doctor8.e99223a0.svg"
+  },
+  {
+    "revision": "fda680d1f670a99fc3ae020f7165af71",
+    "url": "/static/media/graduation.fda680d1.svg"
+  },
+  {
+    "revision": "21ba49787778bb0410cc7858476cd8cd",
+    "url": "/static/media/mention.21ba4978.svg"
+  },
+  {
+    "revision": "45a94407e4c5011b827e102bd3f12304",
+    "url": "/static/media/newDoctor1.45a94407.svg"
+  },
+  {
+    "revision": "f90bc4718980aba88ed2ea4bf41eaa8b",
+    "url": "/static/media/preloader.f90bc471.gif"
+  },
+  {
+    "revision": "4e091d0b79498592e7eb9dc896f16e64",
+    "url": "/static/media/undraw_doctor_kw5l.4e091d0b.svg"
+  },
+  {
+    "revision": "6e6a5db0321648df934dffb544d7036d",
+    "url": "/static/media/undraw_forms_78yw.6e6a5db0.svg"
+  },
+  {
+    "revision": "41b3f391a309f8f9f81a0c30170d0c1f",
+    "url": "/static/media/undraw_medical_research_qg4d.41b3f391.svg"
+  }
+]);
